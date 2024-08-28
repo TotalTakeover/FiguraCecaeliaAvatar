a088b319764e326dc12175bd70c051e31ec9dadb
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-08-27 22:57:56 -0700

    Update Player.lua
    - Added updates from the Merling avatar
    - Implemented PartsAPI
      - Full implementation later

--- scripts/Player.lua
+++ scripts/Player.lua
@@ -1,187 +1,156 @@
---Configures the ModelParts that mimic vanilla parts.
+-- Required script
+local parts = require("lib.PartsAPI")
 
---Change this if you change the bbmodel's name
-local model = models.Cecaelia
---Minor optimization. Saves like 20 instructions lol
-local modelRoot = model.Player
+-- Config setup
+config:name("Cecaelia")
+local vanillaSkin = config:load("AvatarVanillaSkin")
+local slim        = config:load("AvatarSlim") or false
+if vanillaSkin == nil then vanillaSkin = true end
 
-local vanillaSkinParts = {
-  modelRoot.Head.Head,
-  modelRoot.Head.Hat_Layer,
+-- Reenabled parts
+--parts.group.LeftLeg :visible(true)
+--parts.group.RightLeg:visible(true)
+--parts.group.Skull   :visible(true)
+--parts.group.Portrait:visible(true)
 
-  modelRoot.Body.Body,
-  modelRoot.Body.Body_Layer,
+-- Arm parts
+local defaultParts = parts:createTable(function(part) return part:getName():find("ArmDefault") end)
+local slimParts    = parts:createTable(function(part) return part:getName():find("ArmSlim")    end)
 
-  modelRoot.RightArm.RightSteve,
-  modelRoot.RightArm.RightSlim,
+-- Vanilla skin parts
+local skinParts = parts:createTable(function(part) return part:getName():find("_Skin") end)
 
-  modelRoot.LeftArm.LeftSteve,
-  modelRoot.LeftArm.LeftSlim,
-}
-for _, part in pairs(vanillaSkinParts) do
-  part:setPrimaryTexture(require("Config").usesPlayerSkin and "SKIN" or nil)
+-- Layer parts
+local layerTypes = {"HAT", "JACKET", "LEFT_SLEEVE", "RIGHT_SLEEVE", "LEFT_PANTS_LEG", "RIGHT_PANTS_LEG", "CAPE", "TAIL_LAYER"}
+local layerParts = {}
+for _, type in pairs(layerTypes) do
+	layerParts[type] = parts:createTable(function(part) return part:getName():find(type) end)
 end
 
-modelRoot.Body.Cape:setPrimaryTexture(require("Config").usesPlayerSkin and "CAPE" or nil)
+-- Determine vanilla player type on init
+local vanillaAvatarType
+function events.ENTITY_INIT()
+	
+	vanillaAvatarType = player:getModelType()
+	
+end
 
---Sets the modelType of the avatar.
-local slim = require("Config").isSlim or false
-modelRoot.LeftArm.LeftSteve:setVisible(not slim and nil)
-modelRoot.RightArm.RightSteve:setVisible(not slim and nil)
+function events.RENDER(delta, context)
+	
+	-- Model shape
+	local slimShape = (vanillaSkin and vanillaAvatarType == "SLIM") or (slim and not vanillaSkin)
+	for _, part in ipairs(defaultParts) do
+		part:visible(not slimShape)
+	end
+	for _, part in ipairs(slimParts) do
+		part:visible(slimShape)
+	end
+	
+	-- Skin textures
+	local skinType = vanillaSkin and "SKIN" or "PRIMARY"
+	for _, part in ipairs(skinParts) do
+		part:primaryTexture(skinType)
+	end
+	
+	-- Cape textures
+	parts.group.Cape:primaryTexture(vanillaSkin and "CAPE" or "PRIMARY")
+	
+	-- Layer toggling
+	for layerType, parts in pairs(layerParts) do
+		local enabled
+		if layerType == "TAIL_LAYER" then
+			enabled = player:isSkinLayerVisible("RIGHT_PANTS_LEG") or player:isSkinLayerVisible("LEFT_PANTS_LEG")
+		else
+			enabled = player:isSkinLayerVisible(layerType)
+		end
+		for _, part in ipairs(parts) do
+			part:visible(enabled)
+		end
+	end
+	
+end
 
-modelRoot.LeftArm.LeftSlim:setVisible(slim and nil)
-modelRoot.RightArm.RightSlim:setVisible(slim and nil)
+-- Vanilla skin toggle
+function pings.setAvatarVanillaSkin(boolean)
+	
+	vanillaSkin = boolean
+	config:save("AvatarVanillaSkin", vanillaSkin)
+	
+end
 
---Show/hide skin layers depending on Skin Customization settings
----@type table<PlayerAPI.skinLayer|string,ModelPart[]>
-local layerParts = {
-  HAT = {
-    modelRoot.Head.Hat_Layer,
-  },
-  JACKET = {
-    modelRoot.Body.Body_Layer,
-  },
-  RIGHT_SLEEVE = {
-    modelRoot.RightArm.RightSteve.RightArmLayerSteve,
-    modelRoot.RightArm.RightSlim.RightArmLayerSlim,
-  },
-  LEFT_SLEEVE = {
-    modelRoot.LeftArm.LeftSteve.LeftArmLayerSteve,
-    modelRoot.LeftArm.LeftSlim.LeftArmLayerSlim,
-  },
-  CAPE = {
-    modelRoot.Body.Cape,
-  },
-  TAIL = {
-    modelRoot.Tail.LowerBodyLayer,
-    modelRoot.Tail.LowerBodyLayer_Glow,
-    -- Tentacle 1
-    modelRoot.Tail.Tentacle1.Layer,
-    modelRoot.Tail.Tentacle1.Layer_Glow,
-    modelRoot.Tail.Tentacle1.T1_1.Layer,
-    modelRoot.Tail.Tentacle1.T1_1.Layer_Glow,
-    modelRoot.Tail.Tentacle1.T1_1.T1_2.Layer,
-    modelRoot.Tail.Tentacle1.T1_1.T1_2.Layer_Glow,
-    modelRoot.Tail.Tentacle1.T1_1.T1_2.T1_3.Layer,
-    modelRoot.Tail.Tentacle1.T1_1.T1_2.T1_3.Layer_Glow,
-    modelRoot.Tail.Tentacle1.T1_1.T1_2.T1_3.T1_4.Layer,
-    modelRoot.Tail.Tentacle1.T1_1.T1_2.T1_3.T1_4.Layer_Glow,
-    modelRoot.Tail.Tentacle1.T1_1.T1_2.T1_3.T1_4.T1_5.Layer,
-    modelRoot.Tail.Tentacle1.T1_1.T1_2.T1_3.T1_4.T1_5.Layer_Glow,
-    -- Tentacle 2
-    modelRoot.Tail.Tentacle2.Layer,
-    modelRoot.Tail.Tentacle2.Layer_Glow,
-    modelRoot.Tail.Tentacle2.T2_1.Layer,
-    modelRoot.Tail.Tentacle2.T2_1.Layer_Glow,
-    modelRoot.Tail.Tentacle2.T2_1.T2_2.Layer,
-    modelRoot.Tail.Tentacle2.T2_1.T2_2.Layer_Glow,
-    modelRoot.Tail.Tentacle2.T2_1.T2_2.T2_3.Layer,
-    modelRoot.Tail.Tentacle2.T2_1.T2_2.T2_3.Layer_Glow,
-    modelRoot.Tail.Tentacle2.T2_1.T2_2.T2_3.T2_4.Layer,
-    modelRoot.Tail.Tentacle2.T2_1.T2_2.T2_3.T2_4.Layer_Glow,
-    modelRoot.Tail.Tentacle2.T2_1.T2_2.T2_3.T2_4.T2_5.Layer,
-    modelRoot.Tail.Tentacle2.T2_1.T2_2.T2_3.T2_4.T2_5.Layer_Glow,
-    -- Tentacle 3
-    modelRoot.Tail.Tentacle3.Layer,
-    modelRoot.Tail.Tentacle3.Layer_Glow,
-    modelRoot.Tail.Tentacle3.T3_1.Layer,
-    modelRoot.Tail.Tentacle3.T3_1.Layer_Glow,
-    modelRoot.Tail.Tentacle3.T3_1.T3_2.Layer,
-    modelRoot.Tail.Tentacle3.T3_1.T3_2.Layer_Glow,
-    modelRoot.Tail.Tentacle3.T3_1.T3_2.T3_3.Layer,
-    modelRoot.Tail.Tentacle3.T3_1.T3_2.T3_3.Layer_Glow,
-    modelRoot.Tail.Tentacle3.T3_1.T3_2.T3_3.T3_4.Layer,
-    modelRoot.Tail.Tentacle3.T3_1.T3_2.T3_3.T3_4.Layer_Glow,
-    modelRoot.Tail.Tentacle3.T3_1.T3_2.T3_3.T3_4.T3_5.Layer,
-    modelRoot.Tail.Tentacle3.T3_1.T3_2.T3_3.T3_4.T3_5.Layer_Glow,
-    -- Tentacle 4
-    modelRoot.Tail.Tentacle4.Layer,
-    modelRoot.Tail.Tentacle4.Layer_Glow,
-    modelRoot.Tail.Tentacle4.T4_1.Layer,
-    modelRoot.Tail.Tentacle4.T4_1.Layer_Glow,
-    modelRoot.Tail.Tentacle4.T4_1.T4_2.Layer,
-    modelRoot.Tail.Tentacle4.T4_1.T4_2.Layer_Glow,
-    modelRoot.Tail.Tentacle4.T4_1.T4_2.T4_3.Layer,
-    modelRoot.Tail.Tentacle4.T4_1.T4_2.T4_3.Layer_Glow,
-    modelRoot.Tail.Tentacle4.T4_1.T4_2.T4_3.T4_4.Layer,
-    modelRoot.Tail.Tentacle4.T4_1.T4_2.T4_3.T4_4.Layer_Glow,
-    modelRoot.Tail.Tentacle4.T4_1.T4_2.T4_3.T4_4.T4_5.Layer,
-    modelRoot.Tail.Tentacle4.T4_1.T4_2.T4_3.T4_4.T4_5.Layer_Glow,
-    -- Tentacle 5
-    modelRoot.Tail.Tentacle5.Layer,
-    modelRoot.Tail.Tentacle5.Layer_Glow,
-    modelRoot.Tail.Tentacle5.T5_1.Layer,
-    modelRoot.Tail.Tentacle5.T5_1.Layer_Glow,
-    modelRoot.Tail.Tentacle5.T5_1.T5_2.Layer,
-    modelRoot.Tail.Tentacle5.T5_1.T5_2.Layer_Glow,
-    modelRoot.Tail.Tentacle5.T5_1.T5_2.T5_3.Layer,
-    modelRoot.Tail.Tentacle5.T5_1.T5_2.T5_3.Layer_Glow,
-    modelRoot.Tail.Tentacle5.T5_1.T5_2.T5_3.T5_4.Layer,
-    modelRoot.Tail.Tentacle5.T5_1.T5_2.T5_3.T5_4.Layer_Glow,
-    modelRoot.Tail.Tentacle5.T5_1.T5_2.T5_3.T5_4.T5_5.Layer,
-    modelRoot.Tail.Tentacle5.T5_1.T5_2.T5_3.T5_4.T5_5.Layer_Glow,
-    -- Tentacle 6
-    modelRoot.Tail.Tentacle6.Layer,
-    modelRoot.Tail.Tentacle6.Layer_Glow,
-    modelRoot.Tail.Tentacle6.T6_1.Layer,
-    modelRoot.Tail.Tentacle6.T6_1.Layer_Glow,
-    modelRoot.Tail.Tentacle6.T6_1.T6_2.Layer,
-    modelRoot.Tail.Tentacle6.T6_1.T6_2.Layer_Glow,
-    modelRoot.Tail.Tentacle6.T6_1.T6_2.T6_3.Layer,
-    modelRoot.Tail.Tentacle6.T6_1.T6_2.T6_3.Layer_Glow,
-    modelRoot.Tail.Tentacle6.T6_1.T6_2.T6_3.T6_4.Layer,
-    modelRoot.Tail.Tentacle6.T6_1.T6_2.T6_3.T6_4.Layer_Glow,
-    modelRoot.Tail.Tentacle6.T6_1.T6_2.T6_3.T6_4.T6_5.Layer,
-    modelRoot.Tail.Tentacle6.T6_1.T6_2.T6_3.T6_4.T6_5.Layer_Glow,
-    -- Tentacle 7
-    modelRoot.Tail.Tentacle7.Layer,
-    modelRoot.Tail.Tentacle7.Layer_Glow,
-    modelRoot.Tail.Tentacle7.T7_1.Layer,
-    modelRoot.Tail.Tentacle7.T7_1.Layer_Glow,
-    modelRoot.Tail.Tentacle7.T7_1.T7_2.Layer,
-    modelRoot.Tail.Tentacle7.T7_1.T7_2.Layer_Glow,
-    modelRoot.Tail.Tentacle7.T7_1.T7_2.T7_3.Layer,
-    modelRoot.Tail.Tentacle7.T7_1.T7_2.T7_3.Layer_Glow,
-    modelRoot.Tail.Tentacle7.T7_1.T7_2.T7_3.T7_4.Layer,
-    modelRoot.Tail.Tentacle7.T7_1.T7_2.T7_3.T7_4.Layer_Glow,
-    modelRoot.Tail.Tentacle7.T7_1.T7_2.T7_3.T7_4.T7_5.Layer,
-    modelRoot.Tail.Tentacle7.T7_1.T7_2.T7_3.T7_4.T7_5.Layer_Glow,
-    -- Tentacle 8
-    modelRoot.Tail.Tentacle8.Layer,
-    modelRoot.Tail.Tentacle8.Layer_Glow,
-    modelRoot.Tail.Tentacle8.T8_1.Layer,
-    modelRoot.Tail.Tentacle8.T8_1.Layer_Glow,
-    modelRoot.Tail.Tentacle8.T8_1.T8_2.Layer,
-    modelRoot.Tail.Tentacle8.T8_1.T8_2.Layer_Glow,
-    modelRoot.Tail.Tentacle8.T8_1.T8_2.T8_3.Layer,
-    modelRoot.Tail.Tentacle8.T8_1.T8_2.T8_3.Layer_Glow,
-    modelRoot.Tail.Tentacle8.T8_1.T8_2.T8_3.T8_4.Layer,
-    modelRoot.Tail.Tentacle8.T8_1.T8_2.T8_3.T8_4.Layer_Glow,
-    modelRoot.Tail.Tentacle8.T8_1.T8_2.T8_3.T8_4.T8_5.Layer,
-    modelRoot.Tail.Tentacle8.T8_1.T8_2.T8_3.T8_4.T8_5.Layer_Glow,
-  },
-}
----@type table<string,PlayerAPI.skinLayer[]>
-local customPartType = {
-  TAIL = {
-    "RIGHT_PANTS_LEG",
-    "LEFT_PANTS_LEG",
-  },
-}
+-- Model type toggle
+function pings.setAvatarModelType(boolean)
+	
+	slim = boolean
+	config:save("AvatarSlim", slim)
+	
+end
+
+-- Sync variables
+function pings.syncPlayer(a, b)
+	
+	vanillaSkin = a
+	slim = b
+	
+end
+
+-- Host only instructions
+if not host:isHost() then return end
+
+-- Required scripts
+local itemCheck = require("lib.ItemCheck")
+local s, color = pcall(require, "scripts.ColorProperties")
+if not s then color = {} end
+
+-- Sync on tick
 function events.TICK()
-  for playerPart, parts in pairs(layerParts) do
-    local enabled = true
-    if customPartType[playerPart] then
-      for _, layer in ipairs(customPartType[playerPart]) do
-        enabled = enabled and player:isSkinLayerVisible(layer)
-      end
-    else
-      enabled = player:isSkinLayerVisible(playerPart)
-    end
-    enabled = enabled and nil
-    for _, part in ipairs(parts) do
-      part:setVisible(enabled)
-    end
-  end
-  modelRoot.Tail:setVisible(player:getGamemode() ~= "SPECTATOR")
+	
+	if world.getTime() % 200 == 0 then
+		pings.syncPlayer(vanillaSkin, slim)
+	end
+	
 end
+
+-- Table setup
+local t = {}
+
+-- Actions
+t.vanillaSkinAct = action_wheel:newAction()
+	:item(itemCheck("player_head{SkullOwner:"..avatar:getEntityName().."}"))
+	:onToggle(pings.setAvatarVanillaSkin)
+	:toggled(vanillaSkin)
+
+t.modelAct = action_wheel:newAction()
+	:item(itemCheck("player_head"))
+	:toggleItem(itemCheck("player_head{SkullOwner:MHF_Alex}"))
+	:onToggle(pings.setAvatarModelType)
+	:toggled(slim)
+
+-- Update actions
+function events.RENDER(delta, context)
+	
+	if action_wheel:isEnabled() then
+		t.vanillaSkinAct
+			:title(toJson
+				{"",
+				{text = "Toggle Vanilla Texture\n\n", bold = true, color = color.primary},
+				{text = "Toggles the usage of your vanilla skin.", color = color.secondary}}
+			)
+		
+		t.modelAct
+			:title(toJson
+				{"",
+				{text = "Toggle Model Shape\n\n", bold = true, color = color.primary},
+				{text = "Adjust the model shape to use Default or Slim Proportions.\nWill be overridden by the vanilla skin toggle.", color = color.secondary}}
+			)
+		
+		for _, page in pairs(t) do
+			page:hoverColor(color.hover):toggleColor(color.active)
+		end
+		
+	end
+	
+end
+
+-- Return actions
+return t
