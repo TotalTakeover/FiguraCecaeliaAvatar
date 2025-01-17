d43d1b0649e1465d1783873792a14778406bda70
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-01-17 01:31:01 -0700

    GSAnimBlend Update

--- scripts/Anims.lua
+++ scripts/Anims.lua
@@ -232,7 +232,7 @@ local blendAnims = {
 
 -- Apply GS Blending
 for _, blend in ipairs(blendAnims) do
-	blend.anim:blendTime(table.unpack(blend.ticks)):onBlend("easeOutQuad")
+	blend.anim:blendTime(table.unpack(blend.ticks)):blendCurve("easeOutQuad")
 end
 
 -- Fixing spyglass jank
